927455e7eae1e6786f32b412b220aee387fee06b
matthausen <matteo.fusilli@hotmail.it>
2020-12-17 10:45:52 +0000

    user and feed service

--- udagram-api/feed/src/aws.ts
+++ udagram-api/feed/src/aws.ts
@@ -1,35 +1,36 @@
-import AWS = require('aws-sdk');
-import {config} from './config/config';
+import AWS = require('aws-sdk')
+import { config } from './config/config'
 
-
-// Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+if (process.env.ENV === 'dev') {
+  // Configure AWS
+  const credentials = new AWS.SharedIniFileCredentials({ profile: 'default' })
+  AWS.config.credentials = credentials
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: config.aws_region,
-  params: {Bucket: config.aws_media_bucket},
-});
+  params: { Bucket: config.aws_media_bucket },
+})
 
 // Generates an AWS signed URL for retrieving objects
-export function getGetSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
+export function getGetSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5
 
   return s3.getSignedUrl('getObject', {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
-  });
+  })
 }
 
 // Generates an AWS signed URL for uploading objects
-export function getPutSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
+export function getPutSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5
 
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
-  });
+  })
 }
